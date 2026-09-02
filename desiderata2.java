f753cee3f7331e8abd70990067af1ee41a1dc32e
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-02 23:48:33 +0800

    Activity 2.2 desiderata label the different Java application parts.

---
+++ desiderata2.java
@@ -0,0 +1,24 @@
+// Class declaration (pangalan ng class = desiderata)
+public class desiderata {
+// Main method 
+    public static void main(String[] args) {
+// Print statements (naglalabas ng bawat linya ng  ipriprint/string literals sa loob ng “ ”) 
+        System.out.println("Go placidly amid the noise and haste,");
+        System.out.println("and remember what peace there may be in silence.");
+        System.out.println("As far as possible, without surrender,");
+        System.out.println("be on good terms with all persons. Speak your truth quietly and ignorant");
+        System.out.println("clearly; and listen to others, even to the dull and the 
+        System.out.println("they too have their story.");
+        System.out.println("Avoid loud and aggressive person.")
+    }
+}
+
+Class declaration → public class desiderata
+Main method  → public static void main(String[] args){
+Print statements → System.out.println(“......”);
+String literal → “......”
+Comment → // explanation lang hindi kasama sa output
+Curly braces → { start of block 
+Closing curly braces → } start of block 
+Closing curly braces → } start of block 
+Closing curly braces → } end of block
