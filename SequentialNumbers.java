8ca66bdbe2e156738986cfe5165973a32a93a6a7
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-02 23:57:23 +0800

    Activity 2.4 SequentialNumbers

---
+++ SequentialNumbers.java
@@ -0,0 +1,11 @@
+public class SequentialNumbers {
+    public static void main(String[] args) {
+        int sequence1 = 123;
+        int sequence2 = 345;
+        int sequence3 = 789;
+    System.out.println(“Sequential Numbers “);
+    System.out.println(sequence1);
+    System.out.println(sequence2);
+    System.out.println(sequence3);
+   }
+}
