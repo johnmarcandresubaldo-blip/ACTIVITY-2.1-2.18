df29f887b80a6d65b4b1ee548f73debb6b7e61ba
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-03 00:02:19 +0800

    Activity 2.5 Message

---
+++ Message.java
@@ -0,0 +1,13 @@
+// John Marc A. Ubaldo
+// BSIT 1-1
+
+public class Message {
+    public static void main(String[] args) { 
+        String Message1 = “I love my course”;
+        String Message2 = “I love Java programming”;
+        String Message3 = “I love want to be a programmer”;
+    System.out.println(Message1);
+    System.out.println(Message2);
+    System.out.println(Message3);
+   }
+}
