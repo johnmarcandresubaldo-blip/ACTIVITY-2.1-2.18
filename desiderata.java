1829c05e2789e080ba06c14ce074f8be887eccc4
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-02 23:34:17 +0800

    Activity 2.1 desiderata

---
+++ desiderata.java
@@ -0,0 +1,11 @@
+public class desiderata {    
+    public static void main(String[] args) { 
+        System.out.println("Go placidly amid the noise and haste,");
+        System.out.println("and remember what peace there may be in silence.");
+        System.out.println("As far as possible, without surrender,");
+        System.out.println("be on good terms with all persons. Speak your truth quietly and ignorant");
+        System.out.println("clearly; and listen to others, even to the dull and the 
+        System.out.println("they too have their story.");
+        System.out.println("Avoid loud and aggressive person.");
+    }
+}
