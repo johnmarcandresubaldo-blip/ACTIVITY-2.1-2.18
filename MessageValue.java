5ad2fd487afba8e1c82071ec12c9411a1a8c7777
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-06 21:15:15 +0800

    Activity 2.6 MessageValue

---
+++ MessageValue.java
@@ -0,0 +1,11 @@
+// John Marc Ubaldo
+// BSIT Netsec 1-1
+
+public class MessageVlaue {
+    public static void main(Strng[] args) {
+        String Message1 = “The value of pie is:”;
+        double Value = 3.1415;
+    System.out.println(Message1);
+    System.out.println(Value);
+   }
+}
