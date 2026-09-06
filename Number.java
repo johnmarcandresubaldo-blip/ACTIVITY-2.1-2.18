e5a3ae5e3d028a31422bca1699383f1689a7cf5a
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-06 22:09:47 +0800

    Activity 2.16 Number

---
+++ Number.java
@@ -0,0 +1,15 @@
+// John Marc Ubaldo
+// BSIT Netsec 1-1
+
+public class Number {
+    public static void main(String[] args) {
+      
+        System.out.println("Number\tSquare\tCube\n");
+        for (int i = 0 ; i <= 3 ; i++) {
+            int square = i * i;
+            int cube = i * i * i;
+          
+        System.out.println( i + "\t" + square + "\t" + cube);
+        } 
+    }
+}
