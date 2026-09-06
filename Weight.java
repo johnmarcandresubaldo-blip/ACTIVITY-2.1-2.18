7587a9830715298df2a7069242a1a02ed4e137dc
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-06 21:20:58 +0800

    Activity 2.8 Weight

---
+++ Weight.java
@@ -0,0 +1,10 @@
+// John Marc Ubaldo
+// BSIT Netsec 1-1
+
+public class Weight {
+    public static void main(String[] args);
+        int pounds = 1;
+        double kilograms = 0.454;
+    System.out.println(pounds + “pound = “ +  kilograms  +  “kilograms”);
+   }
+}        
