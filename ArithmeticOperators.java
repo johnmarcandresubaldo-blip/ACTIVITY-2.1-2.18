dea39fb93761bd430ae04568187c7e31353c116f
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-06 22:00:28 +0800

    Activity 2.14 ArithmeticOperators

---
+++ ArithmeticOperators.java
@@ -0,0 +1,37 @@
+// John Marc Ubaldo
+// BSIT Netsec 1-1
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int i = 37;
+        int j = 42;
+        double x = 27.475;
+        double y = 7.22;
+        
+       System.out.println("Variable value");
+       System.out.println("Adding");
+       System.out.println("i + j = " + (i + j));
+       System.out.println("x + y = " + (x + y));
+
+       System.out.println("Subtracting...");
+       System.out.println("i - j = " + (i - j));
+       System.out.println("x - y = " + (x - y));
+
+       System.out.println("Multiplying...");
+       System.out.println("i * j = " + (i * j));
+       System.out.println("x * y = " + (x * y));
+      
+       System.out.println("Dividing...");
+       System.out.println("i / j = " + (i / j));
+       System.out.println("x / y = " + (x / y));
+  
+       System.out.println("Computing the remainder...");
+       System.out.println("i % j = " + (i % j));
+       System.out.println("x % y = " + (x % y));
+      
+       System.out.println("Mixing types...");
+       System.out.println("j + y = " + (i + j));
+       System.out.println("i * x = " + (x * y));
+      
+    }
+}
