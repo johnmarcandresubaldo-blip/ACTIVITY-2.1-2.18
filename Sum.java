d8050a4b0767aafdc20422b38bdff2bf546280ea
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-06 21:51:05 +0800

    Activity 2.13 Sum

---
+++ Sum.java
@@ -0,0 +1,16 @@
+// John Marc Ubaldo
+// BSIT Netsec 1-1
+
+public class Sum {
+    public static vod main(String[] args) {
+      
+        int num1 = 10;    //first integers
+        int num2 = 20;    //second integers
+        int sum = num1 + num2;
+      
+        System.out.println(“The sum is: “ + sum);
+    }
+}
+
+What is the output of the program? 
+  The sum is: 30
