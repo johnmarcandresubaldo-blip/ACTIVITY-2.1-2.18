a9c011b528656900eec08035f72e80617466f4cd
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-06 21:18:50 +0800

    Activity 2.7 Circle

---
+++ Circle.java
@@ -0,0 +1,10 @@
+// John Marc Ubaldo
+// BSIT Netsec 1-1
+
+public class circle {
+    public static void main(String[] args) {
+        int raduis = 2;
+        double area = raduis * raduis * 3.142;
+    System.out.println(area);
+    }
+}
