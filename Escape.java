1baf2f0613c7cfe62c50660286840f54772352e7
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-06 22:06:03 +0800

    Activity 2.15 Escape

---
+++ Escape.java
@@ -0,0 +1,13 @@
+// John Marc Ubaldo
+// BSIT Netsec 1-1
+
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("First Name\tLast Name\tStudent Number\tPhone Number");
+        System.out.println("John Marc\tUbaldo   \t26-202   \t0993311445");
+    }
+}
+
+What is the output of the program?
+  First Name   Last Name   Student Number   Phone Number  
+  John Marc     Ubaldo        26-202         0993311445
