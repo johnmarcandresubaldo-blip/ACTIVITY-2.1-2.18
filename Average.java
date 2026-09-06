d285e6bab267957299eab79d4c07f9f0333c33d7
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-06 21:47:21 +0800

    Activity 2.12 Average

---
+++ Average.java
@@ -0,0 +1,22 @@
+// John Marc Ubaldo
+// BSIT Netsec 1-1
+
+public class Average {
+    public static void main(String[] Args);
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 45;
+        int average = (num1 + num2 + num3 )/3;
+  
+        System.out.println(“number 1 =” + num1);
+        System.out.println(“number 2 =” + num2);
+        System.out.println(“number 3 =” + num3);
+        System.out.println(“average is =” + average);
+   }
+}
+
+What is the output of the program? 
+  number 1 = 10
+  number 2 = 20
+  number 3 = 45
+  average is = 25
