e3242de6cc32df13d2119499b9e42170b10f99ae
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-06 21:22:56 +0800

    Activity 2.9 Room

---
+++ Room.java
@@ -0,0 +1,11 @@
+// John Marc Ubaldo
+// BSIT Netsec 1-1
+
+public class  Room {
+    public static void main(String[] args);
+        int length = 20;
+        int width = 15;
+        int floorspace = 300;
+    System.out.println(“The floor  space is  ” + length * width + “  square feet”);
+   }
+}
