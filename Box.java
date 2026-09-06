0f057840b16e4e769c8f85911650b27105fc053c
johnmarcandresubaldo-blip <johnmarcandresubaldo@gmail.com>
2026-09-06 22:20:39 +0800

    Activity 2.17 Box

---
+++ Box.java
@@ -0,0 +1,16 @@
+// John Marc Ubaldo
+// BSIT Netsec 1-1
+
+public class Box {
+    public static void main(String[] args) {
+      
+        System.out.println("*********\t     *       ");
+        System.out.println("*       *\t   *   *     ");
+        System.out.println("*       *\t *       *   ");
+        System.out.println("*       *\t*          * ");     
+        System.out.println("*       *\t *       *   "); 
+        System.out.println("*********\t   *   *     ");
+        System.out.println("\t\t     *     ");
+        System.out.println("Box" + "\t\t" + "Diamond");       
+    }
+}
